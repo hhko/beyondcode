90fb4db17d222d098a89ca76274c2ddc0e8429c2
hhko <hyungho.ko@gmail.com>
2024-12-24 10:19:45 +0900

    fix: 실패 제거

--- Template/Backend/Api/Tests/Crop.Hello.Api.Tests.Unit/Class1.cs
+++ Template/Backend/Api/Tests/Crop.Hello.Api.Tests.Unit/Class1.cs
@@ -1,11 +1,11 @@
-﻿namespace Crop.Hello.Api.Tests.Unit;
+﻿//namespace Crop.Hello.Api.Tests.Unit;
 
-public class Class1
-{
-    [Fact]
-    public void Hello()
-    {
-        int x = 0;
-        int y = 2025 / x;
-    }
-}
+//public class Class1
+//{
+//    [Fact]
+//    public void Hello()
+//    {
+//        int x = 0;
+//        int y = 2025 / x;
+//    }
+//}
