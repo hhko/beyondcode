471b107024b1a17e4588565a54268cc61d085602
hhko <hyungho.ko@gmail.com>
2025-02-07 01:24:47 +0900

    fix: 폴더 경로 조정

--- 03-tutorial/ddd-basic/ch01-exploring-a-complex-domain-/DddGym.Tests.Unit/LayerTests/Domain/Abstractions/Constants/Admin.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DddGym.Tests.Unit.LayerTests.Domain.Abstractions.Constants;
-
-internal static class Admin
-{
-    public static readonly Guid Id = Guid.NewGuid();
-}
--- 03-tutorial/ddd-basic/ch01-exploring-a-complex-domain-/DddGym.Tests.Unit/LayerTests/Domain/Abstractions/Constants/Gym.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DddGym.Tests.Unit.LayerTests.Domain.Abstractions.Constants;
-
-internal static class Gym
-{
-    public static readonly Guid Id = Guid.NewGuid();
-}
